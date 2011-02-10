eed965b7284f3640a6b475c5f6276c417ae93dc1
Aslak Johannessen <aslakjo@gmail.com>
2011-02-10 14:46:14 +0100

    fjerner dependencie

--- project/build/ScalaIntroKurs.scala
+++ project/build/ScalaIntroKurs.scala
@@ -10,8 +10,5 @@
 import sbt._
 class ScalaIntroKurs(info: ProjectInfo) extends DefaultProject(info) with IdeaProject
 {
-  val sbtIdeaRepo = "sbtIdeaRepo" at "http://mpeltonen.github.com/maven/"
-  val sbtCommand =  "com.github.mpeltonen" % "sbt-idea-processor" %  "0.1.0"
-
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
 }
