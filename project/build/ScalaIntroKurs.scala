803b8a3f4f296766c3717e617f14329fc5ee7079
Torbjørn Vatn <torbjorn@vatn.org>
2011-02-15 19:48:48 +0100

    Added growlplugin

--- project/build/ScalaIntroKurs.scala
+++ project/build/ScalaIntroKurs.scala
@@ -1,6 +1,6 @@
-
+import com.withoutincident.sbt.GrowlTestListenerPlugin
 import sbt._
 
-class ScalaIntroKurs(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
+class ScalaIntroKurs(info: ProjectInfo) extends DefaultProject(info) with IdeaProject with GrowlTestListenerPlugin {
   val scalatest = "org.scalatest" % "scalatest" % "1.3"
 }
