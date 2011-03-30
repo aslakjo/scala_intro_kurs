d540be262b77dbb7fb35e7233bdb5c2276502902
Aslak Johannessen <aslakjo@gmail.com>
2011-03-30 22:07:53 +0200

    Fjerner growl plugin

--- project/build/ScalaIntroKurs.scala
+++ project/build/ScalaIntroKurs.scala
@@ -1,6 +1,5 @@
-import com.withoutincident.sbt.GrowlTestListenerPlugin
 import sbt._
 
-class ScalaIntroKurs(info: ProjectInfo) extends DefaultProject(info) with IdeaProject with GrowlTestListenerPlugin {
+class ScalaIntroKurs(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
   val scalatest = "org.scalatest" % "scalatest" % "1.3"
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,8 +2,4 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.2.0"
-
-  val growlTest = "com.withoutincident" % "sbt-growl-test" % "0.3.0"
-
-
 }
