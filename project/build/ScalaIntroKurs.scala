aa135209e127fe760ea9b890fde725c5752d92ed
Torbjørn Vatn <torbjorn@vatn.org>
2011-03-30 23:30:27 +0200

    Added a http GET call to the Scoreboard

--- project/build/ScalaIntroKurs.scala
+++ project/build/ScalaIntroKurs.scala
@@ -1,13 +1,17 @@
 import com.withoutincident.sbt.GrowlTestListenerPlugin
+import io.Source
 import sbt._
 
 class ScalaIntroKurs(info: ProjectInfo) extends DefaultProject(info) with IdeaProject with GrowlTestListenerPlugin with ScoreBoardPublish {
   val scalatest = "org.scalatest" % "scalatest" % "1.3"
 
+  val host = "localhost"
+  val teamNavn = "A-Team"
+
   def customTestListener: TestReportListener = new ScoreBoardUpdater(this)
   override def testListeners: Seq[TestReportListener] = super.testListeners ++ Seq(customTestListener)
 
   def publishScore(passed: Int, failed: Int, skipped: Int) = {
-    println("Passed: "+passed+" Failed: "+failed+" Skipped: "+skipped)
+    Source.fromURL ("http://%s:9000/team/%s/passed/%s/failed/%s/skipped/%s" format (host, teamNavn, passed, failed, skipped))
   }
 }
