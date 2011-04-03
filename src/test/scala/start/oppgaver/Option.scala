3c9c3a4b32ef48a77537c708a21d16c9a5ed9297
Aslak Johannessen <aslakjo@gmail.com>
2011-04-03 18:42:09 +0200

    Tar bort fasit fra optionoppgaver

--- src/test/scala/start/oppgaver/Option.scala
+++ src/test/scala/start/oppgaver/Option.scala
@@ -8,23 +8,23 @@ import model._
 class OptionOppgaver extends Spec with ShouldMatchers {
   describe("Option") {
     val team = List("Egon", "Benny", "Kjell")
-    it("skal returnere at vi ikke har det elementet"){
-      def teamMedlem(navn:String) ={
-        team.find(_ == navn)
-      }
+    ignore("skal returnere at vi ikke har det elementet"){
+      def teamMedlem(navn:String) = null
       val element = teamMedlem("Anders")
 
-      element match {
+      /*element match {
         case None => "pass"
         case _ => fail
-      }
+      }*/
     }
 
-    it("skal returnere (ingen) eller navnet på medlemen"){
+    ignore("skal returnere (ingen) eller navnet på medlemen"){
       val benny = Some("Benny")
       val valborg = None
 
-      def navnPåMedlem(medlem : Option[String]) = medlem getOrElse "(ingen)"
+      def navnPåMedlem(medlem : Option[String]) = {
+        "test"
+      }
 
       navnPåMedlem(benny) should be("Benny")
       navnPåMedlem(valborg) should be("(ingen)")
